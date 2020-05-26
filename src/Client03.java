22f588ba649c31db8eef6d9e0ea71d01918da3e5
xinhuanduan <s3713321@student.rmit.edu.au>
2020-05-26 17:19:30 +1000

    c

--- src/Client03.java
+++ src/Client03.java
@@ -2,6 +2,6 @@ import client.Client;
 
 public class Client03 {
     public static void main(String[] args) {
-        Client c1 = new Client("localhost",61128,"localhost", 61573);
+        Client c1 = new Client("netprog1.csit.rmit.edu.au",61128,"localhost", 61573);
     }
 }
